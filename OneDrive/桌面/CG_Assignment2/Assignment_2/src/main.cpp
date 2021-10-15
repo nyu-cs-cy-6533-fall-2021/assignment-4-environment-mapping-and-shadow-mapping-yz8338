1849b4fcc0b14df409d7868ca9925245070f28a1
yz8338 <yz8338@nyu.edu>
2021-10-14 21:38:46 -0400

    update gif

--- OneDrive/桌面/CG_Assignment2/Assignment_2/src/main.cpp
+++ OneDrive/桌面/CG_Assignment2/Assignment_2/src/main.cpp
@@ -72,10 +72,6 @@ glm::vec2 getCurrentWorldPos(GLFWwindow* window) {
     int width, height;
     glfwGetWindowSize(window, &width, &height);
 
-    // // Convert screen position to world coordinates
-    // double xworld = ((xpos/double(width))*2)-1;
-    // double yworld = (((height-1-ypos)/double(height))*2)-1; // NOTE: y axis is flipped in glfw
-
     // Convert screen position to world coordinates
     glm::vec4 p_screen(xpos,height-1-ypos,0,1);
     glm::vec4 p_canonical((p_screen.x/width)*2-1,(p_screen.y/height)*2-1,0,1);
